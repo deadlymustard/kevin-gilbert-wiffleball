2cac0be5c2223d13cb87113489e423209e96ab76
rshaner <regan.shaner@disneystreaming.com>
2021-04-12 08:44:26 -0400

    Added additional pages

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
+## Deploying
+
+Run `firebase login` and login to the correct project.
+
+Run `firebase use kevin-gilbert-wiffleball-<env>` to switch to the environment you'd like to deploy to.
+
+Run `ng deploy` to deploy the code.
+
 ## Code scaffolding
 
 Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
--- src/app/pages/past-tournaments/past-tournaments.component.ts
+++ src/app/pages/past-tournaments/past-tournaments.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Tournament } from './../../interfaces/past-tournaments-page';
 import { Component, OnInit } from '@angular/core';
 import { SinglePageService } from 'src/app/services/single-page.service';
@@ -10,15 +11,27 @@ import { SinglePageService } from 'src/app/services/single-page.service';
 export class PastTournamentsComponent implements OnInit {
 
   tournaments!: Tournament[];
-  selectedPage!: number;
+  selectedYear?: number;
+  selectedPage: number = 0;
+  images!: any[]
 
-  constructor(private singlePageService: SinglePageService) { }
+  constructor(
+    private singlePageService: SinglePageService,
+    private route: ActivatedRoute
+  ) { 
+    this.route.queryParams.subscribe(params => {
+      this.selectedYear = Number(params.get('year'));
+    })
+  }
 
   ngOnInit(): void {
     this.singlePageService.fetch("past-tournaments").subscribe(res => {
       this.tournaments = res;
       this.tournaments.sort((a, b) => a.year - b.year)
-      this.selectedPage = this.tournaments.length - 1;
+      console.log(this.selectedYear);
+      this.selectedPage = this.tournaments.findIndex(tournament => tournament.year === this.selectedYear) - 1;
+      this.images = this.tournaments[this.selectedPage].carousel;
+      console.log(this.tournaments);
     })
   }
 }
