6e8510cbb1b30cd031be9d11ca2f4e5d8b35bcbe
Regan <regdshaner@gmail.com>
2021-04-22 14:03:16 -0400

    Added remaining pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  strapiApiUrl: "https://ktg-wiffleball-strapi.herokuapp.com"
+  strapiApiUrl: "https://ktg-wiffleball-strapi.herokuapp.com",
+  paypalApiKey: "AYd_PiOoMb13TRjG8AQnQFvOLqZIT85fPVxAmLjlBai-N3l9ccju1NgjnMS-KerSm0eMy_YaEd6eK11d"
 };
